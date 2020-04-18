a39db696036764c6a2b7670d6ea2e08efb9b7e42
carrynguyen <tiepnnph07832@fpt.edu.vn>
2020-04-18 15:46:03 +0700

    1 file modified

--- src/app/class.service.ts
+++ src/app/class.service.ts
@@ -8,19 +8,19 @@ export class ClassService {
 
   constructor(private http:HttpClient) {}
 
-    getClassesBySchoolId(schoolId): Observable<any>{
-    let url = `${apiURL}/${schoolId}/classes`;
+    getClassesByhotelsId(hotelId): Observable<any>{
+    let url = `${apiURL}/${hotelId}/classes`;
     return this.http.get<any>(url);
   }
-   getClassesById(schoolId, classesId): Observable<any>{
-    let url = `${apiURL}/${schoolId}/classes/${classesId}`;
+   getClassesById(hotelsId, classesId): Observable<any>{
+    let url = `${apiURL}/${hotelsId}/classes/${classesId}`;
     return this.http.get<any>(url);
   }
-  addNewSchool( obj): Observable<any>{
-    return this.http.post<any>('${apiURL}/${schoolId}/classes', obj);
+  addNewHotels( obj): Observable<any>{
+    return this.http.post<any>('${apiURL}/${hotelsId}/classes', obj);
   }
-  editSchool(obj): Observable<any>{
-    return this.http.put<any> ('${apiURL}/${schoolId}/classes/${obj.id}', obj);
+  editHotel(obj): Observable<any>{
+    return this.http.put<any> ('${apiURL}/${hotelsId}/classes/${obj.id}', obj);
   }
 
   }
